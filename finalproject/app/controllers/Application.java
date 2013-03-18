df3fd39742e1bab54e7ba7047a2b57e0d5f957b9
d2dcontre <c0d3rm4x@gmail.com>
2013-03-19 06:00:40 +0800

    Finished transaction with: 1) Checking for existing item, user 2) Checking for number purchased > 0 3) Checking for number purchased <= current in stock

--- finalproject/app/controllers/Application.java
+++ finalproject/app/controllers/Application.java
@@ -1,16 +1,24 @@
 package controllers;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+
 import javax.sql.DataSource;
+
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.node.ObjectNode;
+
 import play.db.DB;
 import play.libs.Json;
-import play.mvc.*;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller 
 {
@@ -171,13 +179,108 @@ public class Application extends Controller
 			String sql = "SELECT * FROM Transaction";
 			ResultSet rs = s.executeQuery(sql);
 			
-			ArrayList<JsonNode> on = new ArrayList<JsonNode>();
+			Collection<JsonNode> on = new ArrayList<JsonNode>();
 			ObjectNode fin = Json.newObject();
-			doJson(rs,on);
+			//doJson(rs,on);
+			String user,item = "";
+			int noPurch = 0;
+			Date date;
 			
-			Collection<JsonNode> c = on;
-			fin.putArray("transactions").addAll(c);
+			while(rs.next() ) {
+				ObjectNode o = Json.newObject();
+				
+				user = rs.getString(1);
+				item = rs.getString(2);
+				noPurch = rs.getInt(3);
+				date = rs.getDate(4);
+				
+				o.put("user_id", user);
+				o.put("item_id", item);
+				o.put("noPurch", noPurch);
+				o.put("trans_date", date.toString() );
+				
+				on.add(o);
+			}
+			
+			//Collection<JsonNode> c = on;
+			rs.close();
+			
+			fin.putArray("transactions").addAll(on);
 			return ok(fin.toString() );
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			String s = e.getStackTrace().toString();
+			ObjectNode o = Json.newObject();
+			o.put("status", "error");
+			o.put("message", e.getMessage() );
+			
+			return internalServerError(o);
+		}
+	}
+	
+	public static Result purchase(String item, String user, int num) {
+		try {
+			if(num <= 0) {
+				return printMssg("Must purchase more than 0 units!");
+			}
+			
+			DataSource ds = DB.getDataSource();
+			Statement s = ds.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			
+			// check if exist
+			
+			// query for item
+			String sql = "SELECT item_id, number_items from Item where item_id = '" + item + "'";
+			ResultSet rs = s.executeQuery(sql);
+			if(!rs.next() ) {
+				System.out.println("first");
+				return printMssg("item_id does not exist!");
+			} else {
+				System.out.println("second");
+				do {
+					if(!rs.getString(1).equalsIgnoreCase(item)) {
+						System.out.println("third");
+						return printMssg("item_id does not exist!");
+					} else {
+						int updateInt = rs.getInt(2);
+						if(updateInt < num)
+							return printMssg("num purchased must be less than or equal to the current number in stock!");
+					}
+				} while(rs.next() );
+			}
+			
+			// query for user
+			sql = "SELECT user_id from User_Entity where user_id = '" + user + "'";
+			rs = s.executeQuery(sql);
+			if(!rs.next() ) {
+				return printMssg("user_id does not exist!");
+			} else {
+				do {
+					if (!rs.getString(1).equalsIgnoreCase(user))
+						return printMssg("user_id does not exist!");
+				} while (rs.next() );
+			}
+			
+			// if both exist
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			java.util.Date date = new java.util.Date();
+			sql = "INSERT INTO Transaction(user_id,item_id,noPurch,trans_date) values ('" + user + "', '" + item + "', '" + num +"', '" + dateFormat.format(date) + "')";
+			s.executeUpdate(sql);
+			// modify item
+			sql = "SELECT item_id,number_items FROM Item WHERE item_id = '" + item + "'";
+			rs = s.executeQuery(sql);
+			rs.next();
+			int updateInt = rs.getInt(2);
+			updateInt -= num;
+			rs.updateInt(2, updateInt);
+			rs.updateRow();
+			
+			// if not
+			// return error
+			rs.close();
+			
+			return printMssg(item + " successfully purchased by " + user + "! ");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
